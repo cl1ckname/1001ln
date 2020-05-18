22b2ecb17f1bf81bd993869334762cfb93cd92a4
costyashka <20002mc@gmail.com>
2020-05-18 17:43:40 +0300

    test_part1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template, static_file, get, request
+from bottle import route, run, template, static_file, get, request, default_app, Bottle
 from sys import argv
 import os
 @route('/')
@@ -9,15 +9,19 @@ def index():
 def other():
     return template('static/html/other.html')
 
+@route('/test')
+def test():
+    return template('static/html/test.html')
+
 @get("/static/css/<filepath:re:.*\.css>")
 def css(filepath):
     return static_file(filepath, root="static/css")
 
 @route('/result', method='POST')
 def do_login():
-    first = request.forms.get('username')
-    second = request.forms.get('password')
-    return template('static/html/result.html',first=first,seconde=second)
+    first = request.forms.get('radio-button1')
+    second = request.forms.get('radio-button2')
+    return template('static/html/result.html',first=first,second=second)
 
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=argv[1])
