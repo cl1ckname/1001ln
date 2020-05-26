06aae9a3a04e635867a5f12d9b8e6b58bed1792f
costyashka <20002mc@gmail.com>
2020-05-26 14:16:29 +0300

    final

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from bottle import route, run, static_file, get, request
 from lib import *
-from sys import argv
 from ml import solve
 import os
 
@@ -11,10 +10,6 @@ def favicon():
 @route('/')
 def index():
     return template('static/html/index.html', 'DiploDoc')
-
-@route('/other')
-def other():
-    return template('static/html/other.html')
 
 @route('/test')
 def test():
@@ -29,11 +24,14 @@ def css(filepath):
     return static_file(filepath, root="static/css")
 
 @route('/result', method='POST')
-def do_login():
+def testing():
     first = int(request.forms.get('radio-button'))
     second = int(request.forms.get('radio-button1'))
     third = int(request.forms.get('radio-button2'))
-    n = int(solve(first,second,third)[0])
+    fourth = int(request.forms.get('radio-button3'))
+    fifth = int(request.forms.get('radio-button4'))
+    sixth = int(request.forms.get('radio-button5'))
+    n = int(solve(first,second,third,fourth,fifth,sixth)[0])
     answer = d[n]
     print(n,answer)
 
@@ -43,7 +41,4 @@ def do_login():
 def bad_res():
     return template('static/html/result.html','Уйди',answer='Хакер',adress='https://cs.pikabu.ru/post_img/2013/10/27/11/1382897680_599400827.jpg')
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=argv[1])
-else:
-    run(host='localhost', port=8080, debug=True)
+run(host='localhost', port=8080, debug=True)
--- ml.py
+++ ml.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 dino_data = pd.read_csv('./static/db.csv')
-features = ['firstq','secondq','thirdq']
+features = ['firstq','secondq','thirdq','fourthq','fifthq','sixthq']
 X = dino_data[features]
-y = dino_data.answe
+y = dino_data.answer
 
 model = DecisionTreeRegressor(random_state=1)
 model.fit(X,y)
 
-def solve(f,s,t):
-    return model.predict([(f,s,t)])
+def solve(f,s,t,fr,fv,sx):
+    return model.predict([(f,s,t,fr,fv,sx)])
